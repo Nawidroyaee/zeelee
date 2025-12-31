8d88badb1538fe6d2a5f3e284773eb4973773669
Mohammad Hashem <hashimhanify@gmail.com>
2025-12-31 16:21:22 +0430

    a bit of bg color update

--- app/components/sections/menu/MenuSection.tsx
+++ app/components/sections/menu/MenuSection.tsx
@@ -2,8 +2,8 @@ import MainCard from "@/app/components/sections/menu/menuCards/MainCard";
 
 export default function MenuSection() {
   return (
-    <section id="menu" className="bg-gray-100">
-      <div className="container m-auto flex flex-col items-center py-20 justify-center">
+    <section id="menu" className="bg-gray-200 lg:bg-gray-100">
+      <div className="container m-auto px-5 flex flex-col items-center py-5 lg:py-20 justify-center">
         {/* Main meals */}
         <div className="w-full flex flex-wrap items-center justify-center gap-5 lg:gap-10">
           <MainCard />
